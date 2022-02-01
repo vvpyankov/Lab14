e97c3720f298bfef4bde1306a99d32571ec8cc5c
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-02-01 23:18:56 +0300

    Добавьте файлы проекта.

---
+++ lab14abstr/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14abstr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Figure
+    {
+        public abstract double GetArea();
+        public abstract double GetPerimetr();
+        public abstract string Name { get; set; }
+    }
+    class Rectangular : Figure
+    {
+        string name;
+        public double Width { get; set; }
+        public double Heigth { get; set; }
+        public override double GetArea()
+        {
+            return Width * Heigth;
+        }
+        public override double GetPerimetr()
+        {
+            return Width * 2 + Heigth * 2;
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+           
+
+    }
+}
---
+++ lab14zadacha/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Разработать абстрактный класс Animal, который описывает животное. Класс содержит следующие элементы:
+абстрактное свойство - название животного.
+В классе Animal нужно определить следующие методы:
+конструктор, устанавливающий значение по умолчанию для названия;
+абстрактный метод Say(), который выводит звук, который издает животное;
+неабстрактный метод ShowInfo(), который выводит на консоль последовательно название, а затем звук (вызывая метод Say()).
+Разработать классы Cat и Dog, которые реализуют абстрактный класс Animal. В классах реализовать следующие элементы:
+
+свойство – название животного;
+метод Say (), выводящий на экран «Мяу» либо «Гав» соответственно.
+Создайте экземпляры классов Cat и Dog и проверьте их работоспособность.*/
+
+namespace lab14zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Animal
+    {
+       public abstract string animalType { get; set; }
+        public abstract void Say();
+
+        public Animal(string animalType)
+        {
+            this.animalType = animalType;
+        }
+        public string AnimalType
+        {
+            get { return animalType; }
+            set { animalType = value; }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("I am a {0}", AnimalType);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string animalType;
+        public Cat (string animalType)
+        {
+            AnimalType = animalType;
+        }
+        public override string AnimalType
+        {
+            get
+            {
+                return animalType;
+
+            }
+            set
+            {
+                animalType = "Cat";
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Meooow!");
+        }
+    }
+    class Dog : Animal
+    {
+        string animalType;
+        public Dog (string animalType)
+        {
+            AnimalType = animalType;
+        }
+        public override string AnimalType
+        {
+            get
+            {
+                return animalType;
+
+            }
+            set
+            {
+                animalType = "Dog";
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Bow-bow-bow!");
+        }
+    }
+}
